f070248d789ebd135528adf98bcc4b83bb490a75
vise1350 <125251867+vise1350@users.noreply.github.com>
2023-07-27 19:02:58 +0530

    Task 4

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -2,10 +2,33 @@
 import { useState } from "react";
 
 function ExpenseForm(){
-    function ChangeDetail(event){
-        console.log(event.target.value)
+    const[enteredTitle,setEnteredTitle]=useState("")
+    const[enteredAmount,setEnteredAmount]=useState("")
+    const[enteredDate,setEnteredDate]=useState("")
+    const[enteredLocation,setEnteredLocaion]=useState("")
 
-    }
+    function ChangeTitle(event){
+        setEnteredTitle(event.target.value)}
+
+        // console.log(event.target.value)
+        function ChangeAmount(event){
+            setEnteredAmount(event.target.value)
+        }
+
+        function ChangeLocation(event){
+            setEnteredLocaion(event.target.value)
+        }
+        function ChangeDate(event){
+            setEnteredDate(event.target.value)
+        }
+        
+        
+
+
+    
+    
+
+    
     
     return(
 
@@ -13,31 +36,31 @@ function ExpenseForm(){
             <form>
                 <div>
                 <label>Title</label>
-                <input type="text " onChange={ChangeDetail}/>
+                <input type="text " onChange={ChangeTitle}/>
 
                 </div>
                 
                 <div>
                 <label>Amount</label>
-                <input type="number" onChange={ChangeDetail}/>
+                <input type="number" onChange={ChangeAmount}/>
 
                     
                 </div>
 
                 <div>
                 <label>Location</label>
-                <input type="text" onChange={ChangeDetail}/>
+                <input type="text" onChange={ChangeLocation}/>
 
                     
                 </div>
                 <div>
                 <label>Date</label>
-                <input type="date" onChange={ChangeDetail}/>
+                <input type="date" onChange={ChangeDate}/>
 
                     
                 </div>
                 <div>
-                    <button type="Submit">Add Expense</button>
+                    <button type="Submit" >Add Expense</button>
                 </div>
 
 
